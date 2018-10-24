998b66465a6260fb3f85d9159a3366d41c06a9c8
Mike <downrightmike@gmail.com>
2018-10-23 17:34:07 -0700

    Menu stub created

---
+++ Hangman.java
@@ -0,0 +1,27 @@
+        /********************************************************************************************************************
+        * Hangman.java
+        * Authors: Downrightmike, Johnnypoblano, Vicksy
+        * CIS 131
+        * 
+        * 
+        ********************************************************************************************************************/
+        import java.util.Scanner;
+
+        public class Hangman {
+        private static Scanner keyboard = new Scanner(System.in);
+        
+        public static void main(String[] args) { 
+
+        }//end of main
+        public static void manu(){
+            System.out.println("********************************");
+            System.out.println("      Welcome to Hangman ");
+            System.out.println("********************************");
+            System.out.println("");
+            System.out.println("Please select an option:");
+            System.out.println("       1. Short game");
+            System.out.println("       2. Medium game");
+            System.out.println("       3. Long game");
+            
+        }//end of menu method
+        }//end of Hangman class
