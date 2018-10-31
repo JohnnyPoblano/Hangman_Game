c2dcc7da5ac64d6e7c5f339af23f064fa261ba3a
Mike <downrightmike@gmail.com>
2018-10-30 17:37:05 -0700

    High scores now prints a dummy array of scores

--- Hangman.java
+++ Hangman.java
@@ -2,22 +2,25 @@
         * Hangman.java
         * Authors: Downrightmike, Johnnypoblano, Vicksy
         * CIS 131
-        * 
+        * TODO: Ask for Name and Password for high scores.
         * 
         ********************************************************************************************************************/
         import java.util.Scanner;
 
         public class Hangman {
         private static Scanner keyboard = new Scanner(System.in);
-        
+        public static String[][] tempScores = new String[3][5];
         public static void main(String[] args) { 
             //Create an array to hold the game type choices
             int[] gameType = new int[2];// putting this in main so that it can be accessed outside of the menu
+           
             menu(gameType);
 
         }//end of main
 
         public static void menu(int[] gameType){
+
+            //need a loop to play another round.
             String s1 = ""; //Setting to 1 as the default so a player can just hit enter to play
             String s2 = "";
             Scanner input = new Scanner(System.in);// Should replace with IR4
@@ -51,7 +54,7 @@
             
             //Call highScores
             if(s1.equals("5")){
-                highScores();
+                highScores(tempScores);
             }
             else{
             //set the game type into the array
@@ -64,8 +67,26 @@
             }//end of highScores check
         }//end of menu method
 
-        public static void highScores(){//stub
+        public static void highScores(String[][] tempScores){//stub
+            fillTempScores(tempScores);
             System.out .println("High Scores!" );
+            for(int i = 0; i<tempScores.length; i++){
+                for(int j = 0; j<tempScores[1].length; j++){
+                    if(i == 0){                
+                       System.out.println( tempScores[0][j]); //print a name
+                        } else {
+                            System.out.println(tempScores[i][j]); //print a score
+                        } } }
         }//end of highScores
+
+        public static void fillTempScores(String[][] tempScores){
+            for(int i = 0; i<tempScores.length; i++){
+            for(int j = 0; j<tempScores[1].length; j++){
+                if(i == 0){                
+                tempScores[0][j] = "Mike"; //set a name
+                } else {
+                tempScores[i][j] = "30"; //set a score
+                } } }
+        } //end of fillTempScores
 
     }//end of Hangman class
