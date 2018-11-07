c35f05b6d6ce22e4cc8fd7e92dfaf26d64e93143
Mike <downrightmike@gmail.com>
2018-11-06 17:20:30 -0700

    Updated Menu
    Added a save file option and stub method.
    High Scores asks for the player's name.
    Added themes Base Dictionary, Western, Scifi.
    Also using IR4 for input validation.

--- Hangman.java
+++ Hangman.java
@@ -10,6 +10,7 @@
         public class Hangman {
         private static Scanner keyboard = new Scanner(System.in);
         public static String[][] tempScores = new String[3][5];
+        public static String[] playerScores = new String[3];
         public static void main(String[] args) { 
             //Create an array to hold the game type choices
             int[] gameType = new int[2];// putting this in main so that it can be accessed outside of the menu
@@ -21,8 +22,12 @@
         public static void menu(int[] gameType){
 
             //need a loop to play another round.
+            String playerName = "";
             String s1 = ""; //Setting to 1 as the default so a player can just hit enter to play
             String s2 = "";
+            int s_1 = 0;// Setting up temporary ints to be used to get user input with IR4
+            int s_2 = 0;
+            
             Scanner input = new Scanner(System.in);// Should replace with IR4
 
             System.out.println("********************************");
@@ -35,8 +40,9 @@
             System.out.println("       3. Medium game");
             System.out.println("       4. Long game");
             System.out.println("       5. High Scores");
-            s1 = input.nextLine();// Should replace with IR4 and require a input
-
+            System.out.println("       6. Save File");
+            s_1 = IR4.getIntegerBetweenLowAndHigh("Please select a game option number to play: ", 1, 5, "Please select a theme by number from the available options.");
+            s1 = String.valueOf(s_1);
             //skip the next section if it is quick game
             if(s1.equals("1")){
                 s2 = "1";
@@ -44,17 +50,25 @@
             else if(s1.equals("5")) {
                     s2 = "1";            
             } 
+            else if(s1.equals("6")) {
+                    s2 = "0";            
+            } 
             else {
             System.out.println("Please select an theme option:");
-            System.out.println("       1. Theme 1");
-            System.out.println("       2. Theme 2");
-            System.out.println("       3. Theme 3");
-            s2 = input.nextLine();// Should replace with IR4
-            }
+            System.out.println("       1. Base Dictionary");
+            System.out.println("       2. Western");
+            System.out.println("       3. SciFi");
+            s_2 = IR4.getIntegerBetweenLowAndHigh("Please select a theme number to play: ", 1, 3, "Please select a theme by number from the available options.");
+            s2 = String.valueOf(s_2);
+        }
             
             //Call highScores
             if(s1.equals("5")){
-                highScores(tempScores);
+                playerName = IR4.getString("Please enter your name: ");
+                highScores(tempScores, playerName);
+            }
+            if(s1.equals("6")){
+                saveFile();
             }
             else{
             //set the game type into the array
@@ -67,9 +81,10 @@
             }//end of highScores check
         }//end of menu method
 
-        public static void highScores(String[][] tempScores){//stub
-            fillTempScores(tempScores);
-            System.out .println("High Scores!" );
+        public static void highScores(String[][] tempScores, String playerName){//stub
+            fillTempScores(tempScores, playerName);
+            //Using player name 
+            System.out .println("Welcome to High Scores " + playerName + "!\n" );
             for(int i = 0; i<tempScores.length; i++){
                 for(int j = 0; j<tempScores[1].length; j++){
                     if(i == 0){                
@@ -79,14 +94,27 @@
                         } } }
         }//end of highScores
 
-        public static void fillTempScores(String[][] tempScores){
+        public static void fillTempScores(String[][] tempScores, String playerName){
             for(int i = 0; i<tempScores.length; i++){
             for(int j = 0; j<tempScores[1].length; j++){
                 if(i == 0){                
-                tempScores[0][j] = "Mike"; //set a name
+                tempScores[0][j] = playerName; //set a name
                 } else {
                 tempScores[i][j] = "30"; //set a score
                 } } }
         } //end of fillTempScores
+        public static void saveFile(){ //Stub for save file options.
+            System.out.println("Save File Accessed!");
+        }
+
+        public static String getLetters(){//Stub to get the user's guess
+            String letterEntered = "";
+            letterEntered = IR4.getString("Please enter your guesss: ");
+            return letterEntered;
+        }//end of get letters
+
+        public static void printPlayerScore(){
+            //stub
+        }
 
     }//end of Hangman class
